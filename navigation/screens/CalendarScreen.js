42f2ace8056294e7a44d9c80d636e01920786cd1
C-Dog346 <callum@clow.net.nz>
2022-09-10 18:24:16 +1200

    Restarted Calendar

--- navigation/screens/CalendarScreen.js
+++ navigation/screens/CalendarScreen.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { StyleSheet, StatusBar, SafeAreaView, Text } from "react-native";
+import { Calendar } from 'react-native-calendario';
 
 export default function CalendarScreen({ navigation }) {
   return (
