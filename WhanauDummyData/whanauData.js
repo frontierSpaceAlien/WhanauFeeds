b83a9151af261be939633fb3bfac12ed0c10f666
vgx6688 <vgx6688@autuni.ac.nz>
2022-09-12 17:45:06 +1200

    Create whanauData.js
    Dummy data for whanau screen

---
+++ WhanauDummyData/whanauData.js
@@ -0,0 +1,39 @@
+export const WHANAU = [
+  {
+    title: "My Whanau",
+    data: [
+      {
+        firstName: "My",
+        lastName: "Name",
+        role: "Owner",
+      },
+      {
+        firstName: "Someone",
+        lastName: "Else",
+        role: "Member",
+      },
+      {
+        firstName: "Another",
+        lastName: "Person",
+        role: "Admin",
+      },
+    ],
+  },
+  {
+    title: "Another Whanau",
+    data: [
+      {
+        firstName: "Another",
+        lastName: "Name",
+        role: "Owner",
+      },
+      {
+        firstName: "My",
+        lastName: "Name",
+        role: "Member",
+      },
+    ],
+  },
+];
+
+export default WHANAU;
