36ec1be8341bb6f65deb2c40dad31d572bc5cfe7
vgx6688 <vgx6688@autuni.ac.nz>
2022-09-14 17:28:15 +1200

    Update whanauData.js
    Added ID's for each member

--- WhanauDummyData/whanauData.js
+++ WhanauDummyData/whanauData.js
@@ -3,16 +3,19 @@ export const WHANAU = [
     title: "My Whanau",
     data: [
       {
+        id: 1342,
         firstName: "My",
         lastName: "Name",
         role: "Owner",
       },
       {
+        id: 14324,
         firstName: "Someone",
         lastName: "Else",
         role: "Member",
       },
       {
+        id: 1434,
         firstName: "Another",
         lastName: "Person",
         role: "Admin",
@@ -23,11 +26,13 @@ export const WHANAU = [
     title: "Another Whanau",
     data: [
       {
+        id: 341242,
         firstName: "Another",
         lastName: "Name",
         role: "Owner",
       },
       {
+        id: 1342,
         firstName: "My",
         lastName: "Name",
         role: "Member",
