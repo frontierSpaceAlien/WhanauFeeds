226f3475d400ceef58009d04963aeb4feee5c5ab
C-Dog346 <callum@clow.net.nz>
2022-09-16 22:01:33 +1200

    Comment stuff

--- navigation/screens/WhanauScreen.js
+++ navigation/screens/WhanauScreen.js
@@ -1,12 +1,22 @@
-import * as React from "react";
+import React, { useState} from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
-
+import Member from "../../Whanau/AUT Whanau/WhanauMembers";
 // for now, shall have only one whanau
 
 export default function WhanauScreen({ navigation }) {
+  const [memberState, setMember] = useState(Member);
+
+  // change to be more efficient
+  const changeRole = id => {
+    const filteredData = contactState.filter(item => item.id !== id);
+    var arr = [...contactState, {id: id, firstName: first_Name, lastName: last_Name}]
+    setContact(arr);
+  }
+
+
   return (
     <SafeAreaView style={styles.background}>
-      <Text style={styles.title}>AUT Whanau</Text>
+      <Text style={styles.title}>WF Whanau</Text>
     </SafeAreaView>
   );
 }
