599519f08227f992e738605baa02fa5e0f6de6ea
C-Dog346 <callum@clow.net.nz>
2022-10-20 23:00:21 +1300

    :) help

--- Testing/CallumJestTest/RegisterTest.test.js
+++ Testing/CallumJestTest/RegisterTest.test.js
@@ -1,9 +1,9 @@
-import App from "./../../App";
+import MainContainer from "../../navigation/MainContainer";
 
 test('Given that I open the app, the login/register screen should render', () => {
-    render(<App />);
-    const app = screen;
-    expect(app).toBeInTheDocument();
+    render(<MainContainer />);
+    //const app = screen;
+    //expect(app).toBeInTheDocument();
   })
 
 test('Given that a user enters the correct data and presses register, a information should be sent through', () => {
