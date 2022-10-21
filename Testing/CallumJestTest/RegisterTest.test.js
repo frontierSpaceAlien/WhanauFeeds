ede885436fcd65c4388027923fd0cd4c8035a0fe
C-Dog346 <callum@clow.net.nz>
2022-10-21 16:17:48 +1300

    our versioning is so awful

--- Testing/CallumJestTest/RegisterTest.test.js
+++ Testing/CallumJestTest/RegisterTest.test.js
@@ -1,11 +1,13 @@
 import LoginScreen from "../../navigation/screens/LoginScreen";
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react-native";
 
 test('Given that I open the app, the login/register screen should render', () => {
-    
-    //const app = screen;
-    //expect(app).toBeInTheDocument();
+   render(<LoginScreen />);
+   const textInput = screen.getByPlaceholderText(/Email/);
+   expect(textInput).toBeInTheDocument();
   })
 
-test('Given that a user enters the correct data and presses register, a information should be sent through', () => {
+test('Given that a user enters the correct data and presses register, information should be sent through', () => {
     
 })
