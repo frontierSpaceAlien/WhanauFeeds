c1027dcd847ce95c7b3899d32afbcf0ab2b221f4
Jack-Briggs <66038932+Jack-Briggs@users.noreply.github.com>
2022-10-23 21:21:56 +1300

    Shopping list complete

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -12,7 +12,7 @@ import HomeScreen from "./screens/HomeScreen";
 import CalendarScreen from "./screens/CalendarScreen";
 import RecipeScreen from "./screens/RecipeScreen";
 import PeopleScreen from "./screens/PeopleScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import ShoppingListScreen from "./screens/ShoppingListScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import AddFriendScreen from "./screens/AddFriendScreen";
 import InviteWhanauScreen from "./screens/InviteWhanauScreen";
@@ -26,7 +26,7 @@ const calendarName = "Calendar";
 const recipeName = "Recipes";
 const peopleName = "People";
 const profileName = "Profile";
-const settingsName = "Settings";
+const shoppinglistName = "Shopping List";
 const AddFriendName = "Add Friend";
 const InviteWhanauName = "Invite Whanau";
 const OtherProfileName = "Other Profile";
@@ -82,8 +82,8 @@ const BottomNav = () => {
             iconName = focused ? "people" : "people-outline";
           } else if (routeName == profileName) {
             iconName = focused ? "person" : "person-outline";
-          } else if (routeName === settingsName) {
-            iconName = focused ? "settings" : "settings-outline";
+          } else if (routeName === shoppinglistName) {
+            iconName = focused ? "basket" : "basket-outline";
           }
           return <Ionicons name={iconName} size={size} color={color} />;
         },
@@ -103,7 +103,7 @@ const BottomNav = () => {
       <Tab.Screen name={recipeName} component={RecipeScreen} />
       <Tab.Screen name={peopleName} component={PeopleScreen} />
       <Tab.Screen name={profileName} component={ProfileScreen} />
-      <Tab.Screen name={settingsName} component={SettingsScreen} />
+      <Tab.Screen name={shoppinglistName} component={ShoppingListScreen} />
     </Tab.Navigator>
   );
 };
