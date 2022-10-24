71aee7ce6451659f4f203cdac509ab6830d2e488
C-Dog346 <callum@clow.net.nz>
2022-10-24 14:11:30 +1300

    Sign out button done

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,10 +1,29 @@
 import * as React from "react";
 import { StyleSheet, StatusBar, SafeAreaView, Text } from "react-native";
+import { signOut, getAuth } from "firebase/auth/";
+//import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function HomeScreen({ navigation }) {
+  //const navigation = useNavigation();
+
+  const auth = getAuth();
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      navigation.replace("Login")
+    })
+    .catch(error => alert(error.message));
+  }
+
   return (
     <SafeAreaView style={styles.background}>
-      <Text onPress={() => alert("this is the home screen")}>Home Screen</Text>
+      <TouchableOpacity
+      onPress={handleSignOut}
+      style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -15,5 +34,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
