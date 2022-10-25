50606271352f920b07d5c649b74dfb1bb4dffc2d
C-Dog346 <callum@clow.net.nz>
2022-10-25 14:33:40 +1300

    testing done!

--- Testing/CallumTest/Test.test.js
+++ Testing/CallumTest/Test.test.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import { React } from 'react';
 import LoginScreen from '../../navigation/screens/LoginScreen.js'
 import { render } from '@testing-library/react-native';
 
 describe('Test components render for LoginScreen', () => {
-    
+    const { getByTestId, getByText, debug } = render(<LoginScreen />)
+
+    test("Email text field button renders", () =>{
+        expect(getByTestId("email")).toBeCalled
+    });
+
+    test("Email text field button renders", () =>{
+        expect(getByTestId("password")).toBeCalled
+    });
 
     test("Login button renders", () =>{
         expect(getByText("Login")).toBeCalled
     });
 
-    test("Submit button renders", () =>{
-        expect(getByText("Submit")).toBeCalled
-    });
-
-    test("Email text field button renders", () =>{
-        expect(getByTestID("testID")).toBeCalled
-    });
+    test("Register button renders", () =>{
+        expect(getByText("Register")).toBeCalled
+   });    
 })
